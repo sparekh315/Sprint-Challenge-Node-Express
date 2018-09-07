71dc6fd4ea365be6e839c9425612e709576af5c3
Sania Parekh <sparekh@Sanias-MacBook.local>
2018-09-07 15:37:51 -0500

    post and push for actions and projects, MVP complete

--- Routes/Action.js
+++ Routes/Action.js
@@ -9,12 +9,40 @@ router.get('/', (req, res) => {
      .catch(err => res.status(500).json({ error: 'Actions not retrieved'}));
 });
 
-router.post('/', (req, res) => {
-    
+router.get('/:id', (req, res) => {
+    const { id } =  req.params;
+    action 
+    .get(id)
+     .then(action => {
+         if(action) {
+             res.status(200).json(actiom);
+         } else {
+             res.status(404).json({error: 'The action with this ID is not found'});
+         }
+     })
+     .catch(error => res.status(500).json({ error: 'Cannot retrieve action information'}));
+});
+
+
+
+router.post('/',(req, res) => {
+    const { project_id, description, notes } = req.body;
+  
+    if (!project_id && !description && !notes ) res.status(400).json({ error: 'Please provide a project ID, description, and notes for this action' });
+    action
+      .insert({ project_id: project_id, description: description, notes: notes })
+      .then(action => res.status(201).json( action ))
+      .catch(err => res.status(500).json({ error: 'Action not saved' }));
   });
 
   router.put('/:id', (req, res) => {
-
+    const { id } = req.params;
+    const { project_id, description, notes } = req.body;
+  
+    action
+      .update(id, { project_id, description, notes })
+      .then(action => res.status(200).json([action]))
+      .catch(err => res.status(500).json({ error: 'Action with that ID not found' }));
   });
   
 
--- Routes/Project.js
+++ Routes/Project.js
@@ -18,20 +18,32 @@ router.get('/:id', (req, res) => {
          if(project) {
              res.status(200).json(project);
          } else {
-             res.status(404).json({error: 'The user with this ID is not found'});
+             res.status(404).json({error: 'The project with this ID is not found'});
          }
      })
      .catch(error => res.status(500).json({ error: 'Cannot retrieve project information'}));
 });
 
 
-router.post('/', (req, res) => {
-    
-});
+router.post('/',(req, res) => {
+    const { name, description } = req.body;
+  
+    if (!name && !description ) res.status(400).json({ error: 'Please provide a name and description for this project' });
+    project
+      .insert({ name: name, description: description })
+      .then(project => res.status(201).json( project ))
+      .catch(err => res.status(500).json({ error: 'Project not saved' }));
+  });
 
-router.put('/:id', (req, res) => {
-
-});
+  router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, description } = req.body;
+  
+    project
+      .update(id, { name, description })
+      .then(project => res.status(200).json([project]))
+      .catch(err => res.status(500).json({ error: 'Project with that ID not found' }));
+  });
 
 
 router.delete('/:id', (req, res) => {
